ff172e810bfeff9b8bb779ab48a98596b0b088d7
Anees Ahmad <108647632+IaM-AnEeS@users.noreply.github.com>
2023-02-03 20:37:20 +0500

    Add files via upload

---
+++ Complete web developtment boot camp/tut53.js
@@ -0,0 +1,28 @@
+console.log("This is Tutorial 53");
+function greet(name,greetext="Greeting from javascript"){
+    let name1= "name1";
+    console.log(greettext +  " "  +  name);
+    console.log(name + " is a good boy");
+}
+function sum(a,b,c){
+    let d=a + b + c;
+    return d;
+}
+
+let name   =  "NESU";
+let name1  =  "MOJU";
+let name2  =  "FOJI";
+let name3  =  "HARRY";
+let greettext = "Good morning";
+greet(name,greettext);
+greet(name1,greettext);
+greet(name2,greettext);
+greet(name3,greettext);
+// let returnVal= greet(namr3);
+// console.log(returnVal)
+let returnVal =sum (1,2,3);
+console.log(returnVal)
+// console.log(name  + " is a good boy");
+// console.log(name1  + " is a good boy");
+// console.log(name2  + " is a good boy");
+// console.log(name3  + " is a good boy");
